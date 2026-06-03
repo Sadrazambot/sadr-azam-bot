f7ff9c712d80735a40f7037bbc25d24bef3f0dd0
Sadrazambot <Sadrazambot@gmail.com>
2026-06-03 04:21:04 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = 8584140798:AAFhmaJ_PvAKbup_BzXE---2JwNi-tM6v98
+TOKEN = "8584140798:AAFhmaJ_PvAKbup_BzXE---2JwNi-tM6v98"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام 👋 ربات فعاله!")
